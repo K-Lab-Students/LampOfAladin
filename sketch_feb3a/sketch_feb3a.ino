523d1a3c60afce5cc5b89b769650493396be7dc4
DarkSteelD <justjoke.exe@gmail.com>
2023-05-10 16:28:40 +0300

    Update sketch_feb3a.ino

--- sketch_feb3a/sketch_feb3a.ino
+++ sketch_feb3a/sketch_feb3a.ino
@@ -1,5 +1,3 @@
-#include <dummy.h>
-
 #include <Adafruit_NeoPixel.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,31 +6,33 @@
  
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(10, PIN, NEO_GRB + NEO_KHZ800);
- 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(14, PIN, NEO_GRB + NEO_KHZ800);
+ //Adafruit_NeoPixel pixel = Adafruit_NeoPixel(2, PIN, NEO_GRB + NEO_KHZ800);
+
 void setup()
 {
-pixels.begin(); // This initializes the NeoPixel library.
-srand(5);
+  pixels.begin(); // This initializes the NeoPixel library.
+  srand(5);
 }
  
 void loop()
 {
  
 // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-int i,j,k;
-pixels.setPixelColor(0, pixels.Color(i%256,j%256, k%256) ); // Moderately bright green color. 
-pixels.setPixelColor(1, pixels.Color(i%256,j%256, k%256)); // Moderately bright green color.
-pixels.setPixelColor(2, pixels.Color(i%256,j%256, k%256)); // Moderately bright green color. 
-pixels.setPixelColor(3, pixels.Color(i%256,j%256, k%256)); // Moderately bright green color.
-pixels.setPixelColor(4, pixels.Color(i%256,j%256, k%256)); // Moderately bright green color. 
-pixels.setPixelColor(5, pixels.Color(i%256,j%256, k%256)); // Moderately bright green color.
-pixels.setPixelColor(6, pixels.Color(i%256,j%256, k%256)); // Moderately bright green color. 
-pixels.setPixelColor(7, pixels.Color(i%256,j%256, k%256)); // Moderately bright green color.
-pixels.setPixelColor(8, pixels.Color(i%256,j%256, k%256)); // Moderately bright green color. 
-pixels.setPixelColor(9, pixels.Color(i%256,j%256, k%256)); // Moderately bright green color.
 
-pixels.show(); // This sends the updated pixel color to the hardware.
-delay(200); // Delay for a period of time (in milliseconds).
+ 
+  pixels.setPixelColor(0, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.setPixelColor(2, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.setPixelColor(1, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.setPixelColor(3, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.setPixelColor(4, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.setPixelColor(5, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.setPixelColor(6, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.setPixelColor(7, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.setPixelColor(8, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.setPixelColor(9, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.setPixelColor(10, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  delay(200); // Delay for a period of time (in milliseconds).
 
 }
