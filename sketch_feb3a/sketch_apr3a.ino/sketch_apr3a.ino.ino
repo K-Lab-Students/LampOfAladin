cca70e0d841114597d514da0bc1572d0a47477c1
DarkSteelD <justjoke.exe@gmail.com>
2023-06-23 13:37:26 +0300

    23

--- sketch_feb3a/sketch_apr3a.ino/sketch_apr3a.ino.ino
+++ sketch_feb3a/sketch_apr3a.ino/sketch_apr3a.ino.ino
@@ -1,45 +1,46 @@
-// Загружаем библиотеку Wi-Fi
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com  
+*********/
+
+// Load Wi-Fi library
 #include <WiFi.h>
-#include <EEPROM.h>
-#include <Adafruit_NeoPixel.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#define PIN 2
-#define BTN_1 0
-#define BTN_2 4
-// Замените на свой идентификатор и пароль
+
+// Replace with your network credentials
 const char* ssid = "K-Lab";
 const char* password = "allhailklab";
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(10, PIN, NEO_GRB + NEO_KHZ800);
 
-// Номер порта для сервера
+// Set web server port number to 80
 WiFiServer server(80);
 
-// HTTP-запрос
+// Variable to store the HTTP request
 String header;
 
-// текущее состояние кнопки
+// Auxiliar variables to store the current output state
 String output26State = "off";
 String output27State = "off";
 
-// Номера выводов
+// Assign output variables to GPIO pins
 const int output26 = 26;
 const int output27 = 27;
 
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
 void setup() {
   Serial.begin(115200);
-  EEPROM.begin(100);
-  pixels.begin(); // This initializes the NeoPixel library.
-
-  // Настраиваем выводы платы
+  // Initialize the output variables as outputs
   pinMode(output26, OUTPUT);
   pinMode(output27, OUTPUT);
-  // Переводим выводы в LOW
+  // Set outputs to LOW
   digitalWrite(output26, LOW);
   digitalWrite(output27, LOW);
-  EEPROM.begin(8);
-  // Подключаемся к Wi-Fi
+
+  // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -47,7 +48,7 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  // Выводим локальный IP-адрес и запускаем сервер
+  // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
@@ -55,99 +56,98 @@ void setup() {
   server.begin();
 }
 
-void loop() {
-  WiFiClient client = server.available(); // прослушка входящих клиентов
-  if (client) { // Если подключается новый клиент,
-    Serial.println("New Client."); // выводим сообщение
-    String currentLine = "";
-    while (client.connected()) { // цикл, пока есть соединение клиента
-      if (client.available()) { // если от клиента поступают данные,
-        char c = client.read(); // читаем байт, затем
-        Serial.write(c); // выводим на экран
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
         header += c;
-        if (c == '\n') { // если байт является переводом строки
-          // если пустая строка, мы получили два символа перевода строки
-          // значит это конец HTTP-запроса, формируем ответ сервера:
-                   
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
-            // HTTP заголовки начинаются с кода ответа (напр., HTTP / 1.1 200 OK)
-            // и content-type, затем пустая строка:
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-
-            // Включаем или выключаем светодиоды
+            
+            // turns the GPIOs on and off
             if (header.indexOf("GET /26/on") >= 0) {
               Serial.println("GPIO 26 on");
               output26State = "on";
               digitalWrite(output26, HIGH);
-       //       EEPROM.write(BTN_1, output26State);
-       //       EEPROM.commit();
             } else if (header.indexOf("GET /26/off") >= 0) {
               Serial.println("GPIO 26 off");
               output26State = "off";
               digitalWrite(output26, LOW);
-       //       EEPROM.write(BTN_1, output26State);
-       //       EEPROM.commit();
             } else if (header.indexOf("GET /27/on") >= 0) {
               Serial.println("GPIO 27 on");
               output27State = "on";
               digitalWrite(output27, HIGH);
-       //       EEPROM.write(BTN_2, output27State);
-       //       EEPROM.commit();
             } else if (header.indexOf("GET /27/off") >= 0) {
               Serial.println("GPIO 27 off");
               output27State = "off";
               digitalWrite(output27, LOW);
-         //     EEPROM.write(BTN_2, output27State);
-         //     EEPROM.commit();
             }
-            // Формируем веб-страницу на сервере
+            
+            // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS для кнопок
-            // можете менять под свои нужды
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border:  none; color: white; padding: 16px 40px;");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color:#555555;}</style></head>");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
             client.println("<body><h1>ESP32 Web Server</h1>");
-            client.println("<input type=\"range\" min=\"-10\" max=\"10\">");
-            EEPROM.get(BTN_1, output26State);
-            // Выводим текущее состояние кнопок
-            client.println(" <p>GPIO 26 - State " + output26State + "</p>");
-            // Если output26State сейчас off, то выводим надпись ON
-            if (output26State == "off") {
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>GPIO 26 - State " + output26State + "</p>");
+            // If the output26State is off, it displays the ON button       
+            if (output26State=="off") {
               client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            EEPROM.get(BTN_2, output27State);
-            
-            // Аналогично для второй кнопки
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 27  
             client.println("<p>GPIO 27 - State " + output27State + "</p>");
-            if (output27State == "off") {
+            // If the output27State is off, it displays the ON button       
+            if (output27State=="off") {
               client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
             client.println("</body></html>");
-            // HTTP-ответ завершается пустой строкой
+            
+            // The HTTP response ends with another blank line
             client.println();
+            // Break out of the while loop
             break;
-          } else { // если получили новую строку, очищаем currentLine
+          } else { // if you got a newline, then clear currentLine
             currentLine = "";
           }
-        } else if (c != '\r') { // Если получили что-то ещё кроме возврата строки,
-          currentLine += c; // добавляем в конец currentLine
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
         }
       }
     }
-    // Очистим переменную
+    // Clear the header variable
     header = "";
-    // Закрываем соединение
+    // Close the connection
     client.stop();
     Serial.println("Client disconnected.");
     Serial.println("");
--- sketch_feb3a/sketch_feb3a.ino
+++ sketch_feb3a/sketch_feb3a.ino
@@ -1,38 +1,128 @@
+
+#include <WiFi.h>
+#include <Servo.h>
 #include <Adafruit_NeoPixel.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #define PIN 2
- 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+//Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+// GPIO the servo is attached to
+static const int servoPin = 13;
+
+// Replace with your network credentials
+const char* ssid = "K-Lab";
+const char* password = "allhailklab";
+
+// Set web server port number to 80
+WiFiServer server(80);
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(14, PIN, NEO_GRB + NEO_KHZ800);
- //Adafruit_NeoPixel pixel = Adafruit_NeoPixel(2, PIN, NEO_GRB + NEO_KHZ800);
+// Variable to store the HTTP request
+String header;
 
-void setup()
-{
-  pixels.begin(); // This initializes the NeoPixel library.
-  srand(5);
+// Decode HTTP GET value
+String valueString = String(5);
+int pos1 = 0;
+int pos2 = 0;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+void setup() {
+  Serial.begin(115200);
+  pixels.begin();
+  //myservo.attach(servoPin);  // attaches the servo on the servoPin to the servo object
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
- 
-void loop()
-{
- 
-// pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
 
- 
-  pixels.setPixelColor(0, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.setPixelColor(2, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.setPixelColor(1, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.setPixelColor(3, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.setPixelColor(4, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.setPixelColor(5, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.setPixelColor(6, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.setPixelColor(7, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.setPixelColor(8, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.setPixelColor(9, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.setPixelColor(10, pixels.Color(rand()%256,rand()%256, rand()%256)); // Moderately bright green color.
-  pixels.show(); // This sends the updated pixel color to the hardware.
-  delay(200); // Delay for a period of time (in milliseconds).
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
 
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1">");
+            client.println("<link rel="icon" href="data:,"><style> body { text-align: center;font-family: "Trebuchet MS", Arial;margin-left:auto;margin-right:auto; transition: background 0.5s ease;}");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}");
+            client.println(".slider { width: 300px; }</style>");
+            client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
+                     
+            // Web Page
+            // Initialize slider values and colors
+            client.println("<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="icon" href="data:,"><style> body { text-align: center;font-family: "Trebuchet MS", Arial;margin-left:auto;margin-right:auto; transition: background 0.5s ease;}.slider{width: 300px;}</style><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script></head><body><h1>Lamp</h1><p>Position R: <span id="RedColor"></span></p><input type="range" min="0" max="255" class="slider" id="RedSlider" onchange="updateColor()"/><p>Position G: <span id="GreenColor"></span></p><input type="range" min="0" max="255" class="slider" id="GreenSlider" onchange="updateColor()"/><p>Position B: <span id="BlueColor"></span></p><input type="range" min="0" max="255" class="slider" id="BlueSlider" onchange="updateColor()"/><script>var RedSlider = document.getElementById("RedSlider");var RedColor = document.getElementById("RedColor");var GreenSlider = document.getElementById("GreenSlider");var GreenColor = document.getElementById("GreenColor");var BlueSlider = document.getElementById("BlueSlider");var BlueColor = document.getElementById("BlueColor");function updateColor() {var red = RedSlider.value;var green = GreenSlider.value;var blue = BlueSlider.value;RedColor.innerHTML = red;GreenColor.innerHTML = green;BlueColor.innerHTML = blue;var gradient = "linear-gradient(to right, rgb(0,0,0), rgb(" + red + "," + green + "," + blue + "), rgb(255,255,255))";document.body.style.background = gradient;var textColor = getContrastingColor(red, green, blue);document.body.style.color = textColor;}function getContrastingColor(red, green, blue) A{var luminance = (red * 299 + green * 587 + blue * 114) / 1000;return (luminance >= 128) ? "#000000" : "#ffffff";}RedSlider.value = 4;GreenSlider.value = 0;BlueSlider.value = 240;updateColor();function servo(pos) {$.get("/?value=" + pos + "&");{Connection: close};}</script></body></html>");
+                    
+            //GET /?value=180& HTTP/1.1
+            if(header.indexOf("GET /?value=")>=0) {
+              pos1 = header.indexOf('=');
+              pos2 = header.indexOf('&');
+              valueString = header.substring(pos1+1, pos2);
+              
+              //Rotate the servo
+    
+              pixels.setPixelColor(10, pixels.Color(valueString.toInt(),rand()%256, rand()%256)); // Moderately bright green color.
+              pixels.show(); 
+              Serial.println(valueString); 
+            }         
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
